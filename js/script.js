125ba67cba4b637afdb9e964106229686ff9affb
Esatevera <veravinesate@gmail.com>
2024-08-21 10:03:06 +0100

    Video files

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* typing animation*/
 var typed = new Typed (".typing",{
-    strings:["","Web Developer", "Web Designer","Digital Marketing"],
+    strings:["","Graphic Designer", "Digital Marketer","Web Developer"],
     typeSpeed:100,
     BackSpeed: 60,
     loop: true,
